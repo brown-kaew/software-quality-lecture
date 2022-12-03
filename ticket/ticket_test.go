6b61385572182702bd208d1ab2d4e62842c424c2
Kaew Tibkham <kaew.tib@ascendcorp.com>
2022-12-03 21:52:26 +0700

    Apply testable

--- ticket/ticket_test.go
+++ ticket/ticket_test.go
@@ -4,80 +4,30 @@ import "testing"
 
 func TestPrice(t *testing.T) {
 
-	t.Run("should return 0 when age is 0", func(t *testing.T) {
-		want := 0.0
-		var age uint = 0
+	tests := []struct {
+		name string
+		age  uint
+		want float64
+	}{
+		{name: "should return 0 when age is 0", age: 0, want: 0.0},
+		{"should return 0 when age is 3", 3, 0.0},
+		{"should return 15 when age is 4", 4, 15.0},
+		{"should return 15 when age is 15", 15, 15.0},
+		{"should return 30 when age is 16", 16, 30.0},
+		{"should return 30 when age is 50", 50, 30.0},
+		{"should return 5 when age is 51", 51, 5.0},
+	}
 
-		got := price(age)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			var age uint = tt.age
 
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
+			got := price(age)
 
-	t.Run("should return 0 when age is 3", func(t *testing.T) {
-		want := 0.0
-		var age uint = 3
-
-		got := price(age)
-
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
-
-	t.Run("should return 15 when age is 4", func(t *testing.T) {
-		want := 15.0
-		var age uint = 4
-
-		got := price(age)
-
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
-
-	t.Run("should return 15 when age is 15", func(t *testing.T) {
-		want := 15.0
-		var age uint = 15
-
-		got := price(age)
-
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
-
-	t.Run("should return 30 when age is 16", func(t *testing.T) {
-		want := 30.0
-		var age uint = 16
-
-		got := price(age)
-
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
-
-	t.Run("should return 30 when age is 50", func(t *testing.T) {
-		want := 30.0
-		var age uint = 50
-
-		got := price(age)
-
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
-
-	t.Run("should return 5 when age is 51", func(t *testing.T) {
-		want := 5.0
-		var age uint = 51
-
-		got := price(age)
-
-		if want != got {
-			t.Errorf("Want %f but got %f", want, got)
-		}
-	})
+			if want != got {
+				t.Errorf("Want %f but got %f", want, got)
+			}
+		})
+	}
 }
