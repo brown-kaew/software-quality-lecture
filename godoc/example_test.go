41585465d36bb7dd7ce15b585c71137f7b762c68
Kaew Tibkham <kaew.tib@ascendcorp.com>
2022-12-03 22:47:28 +0700

    Add godoc with example

---
+++ godoc/example_test.go
@@ -0,0 +1,12 @@
+package godoc
+
+import "fmt"
+
+func ExampleMinus() {
+	got := Minus(2, 1)
+
+	fmt.Println(got)
+
+	// Output:
+	// 1
+}
---
+++ godoc/minus.go
@@ -0,0 +1,5 @@
+package godoc
+
+func Minus(a, b int) int {
+	return a - b
+}
