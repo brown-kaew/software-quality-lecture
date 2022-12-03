9d2ab402ad70603d292c4d2fbaac88772d0a98cc
Kaew Tibkham <kaew.tib@ascendcorp.com>
2022-12-03 20:51:26 +0700

    Update sum_test.go

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -9,7 +9,7 @@ func TestSum(t *testing.T) {
 		got := sum(1, 2)
 
 		if want != got {
-			t.Errorf("sum(1, 2) = %d; wannt 3", got)
+			t.Errorf("sum(1, 2) = %d; want %d", got, want)
 		}
 	})
 
@@ -19,7 +19,28 @@ func TestSum(t *testing.T) {
 		got := sum(-1, -1)
 
 		if want != got {
-			t.Errorf("sum(-1, -1) = %d; wannt -2", got)
+			t.Errorf("sum(-1, -1) = %d; want %d", got, want)
+		}
+	})
+
+	t.Run("should return 6 when  1, 2, 3", func(t *testing.T) {
+		want := 6
+
+		got := sum(1, 2, 3)
+
+		if want != got {
+			t.Errorf("sum(1, 2, 3) = %d; want %d", got, want)
+		}
+	})
+
+	t.Run("should return 15 when  1, 2, 3, 4, 5", func(t *testing.T) {
+		want := 15
+		nums := []int{1, 2, 3, 4, 5}
+
+		got := sum(nums...)
+
+		if want != got {
+			t.Errorf("sum(1, 2, 3, 4, 5) = %d; want %d", got, want)
 		}
 	})
 }
